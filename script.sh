45fa642f352f1bf2157a44c7dd6e4519996cdbd6
sirinejrad <sirinejrad6@gmail.com>
2023-09-15 17:04:11 +0100

    update

---
+++ script.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#cd "C:/Users/asus/Desktop/cypress-cucumber"
+git add .
+git commit -m "update"
+git push
+#curl -X GET -u admin:admin "http://localhost:8080/job/premier-projet-cypress/build?token=11cf9ebf15c92cb666f3ffdc90ee56a635"
